d94c6a6968793a5e7ad4c71fad7eeb51330a0ab6
David Chang <zeta11235813@gmail.com>
2015-02-08 21:21:16 +0800

    Bridge attribute to React::Event

--- lib/react/event.rb
+++ lib/react/event.rb
@@ -1,6 +1,19 @@
 module React
   class Event
     include Native
+    alias_native :bubbles, :bubbles
+    alias_native :cancelable, :cancelable
+    alias_native :current_target, :currentTarget
+    alias_native :default_prevented, :defaultPrevented
+    alias_native :event_phase, :eventPhase
+    alias_native :is_trusted?, :isTrusted
+    alias_native :native_event, :nativeEvent
+    alias_native :target, :target
+    alias_native :timestamp, :timeStamp
+    alias_native :event_type, :type
+    alias_native :prevent_default, :preventDefault
+    alias_native :stop_propagation, :stopPropagation
+    
     def initialize(native_element)
       @native = native_element
     end
---
+++ spec/event_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+
+describe React::Event do
+  it "should bridge attributes of native SyntheticEvent" do
+    element = React.create_element('div').on(:click) do |event|
+      expect(event.bubbles).to eq(`#{event.to_n}.bubbles`)
+      expect(event.cancelable).to eq(`#{event.to_n}.cancelable`)
+      expect(event.current_target).to eq(`#{event.to_n}.currentTarget`)
+      expect(event.default_prevented).to eq(`#{event.to_n}.defaultPrevented`)
+      expect(event.event_phase).to eq(`#{event.to_n}.eventPhase`)
+      expect(event.is_trusted?).to eq(`#{event.to_n}.isTrusted`)
+      expect(event.native_event).to eq(`#{event.to_n}.nativeEvent`)
+      expect(event.target).to eq(`#{event.to_n}.target`)
+      expect(event.timestamp).to eq(`#{event.to_n}.timeStamp`)
+      expect(event.event_type).to eq(`#{event.to_n}.type`)
+      expect(event).to respond_to(:prevent_default)
+      expect(event).to respond_to(:stop_propagation)
+    end
+    instance = renderElementToDocument(element)
+    simulateEvent(:click, instance)
+  end
+end
