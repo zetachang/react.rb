e9fd9c4b5b9d95157f1bffadcb8ba5c9adfa052c
David Chang <zeta11235813@gmail.com>
2015-02-15 00:04:28 +0800

    Add polling

--- example/react-tutorial/example.rb
+++ example/react-tutorial/example.rb
@@ -3,14 +3,20 @@ require 'opal-jquery'
 require "json"
 require 'react'
 
+class Window
+  def self.set_interval(delay, &block)
+    `window.setInterval(function(){#{block.call}}, #{delay.to_i})`
+  end
+end
+
 class Comment
   include React::Component
   
   def render
     converter = Native(`new Showdown.converter()`)
     raw_markup = converter.makeHtml(params[:children].to_s)
-    React.create_element('div') do
-      [React.create_element('h2') { params[:author] },
+    React.create_element('div', class_name: "comment") do
+      [React.create_element('h2', class_name: "commentAuthor") { params[:author] },
       React.create_element('span', dangerously_set_inner_HTML: {__html: raw_markup}.to_n)]
     end
   end
@@ -20,7 +26,7 @@ class CommentList
   include React::Component
 
   def render
-    React.create_element('div') do
+    React.create_element('div', class_name: "commentList") do
       params[:data].each_with_index.map do |comment, idx|
         React.create_element(Comment, author: comment["author"], key: idx) { comment["text"] }
       end
@@ -28,50 +34,11 @@ class CommentList
   end
 end
 
-class CommentBox
-  include React::Component
-  after_mount :load_comments_from_server
-  define_state(:data) { [] }
-
-  def load_comments_from_server
-    HTTP.get(params[:url]) do |response|
-      if response.ok?
-        self.data = JSON.parse(response.body)
-      else
-        puts "failed with status #{response.status_code}"
-      end
-    end
-  end
-  
-  def handle_comment_submit(comment)
-    comments = self.data
-    comments.push(comment)
-    self.data = comments
-    
-    HTTP.post(params[:url], payload: comment) do |response|
-      if response.ok?
-        self.data = JSON.parse(response.body)
-      else
-        puts "failed with status #{response.status_code}"
-      end
-    end
-  end
-
-  def render
-    React.create_element('div') do
-      [React.create_element('h1') { "Comments" },
-       React.create_element(CommentList, data: self.data),
-       React.create_element(CommentForm).on(:comment_submit) {|c| handle_comment_submit(c) }]
-    end
-  end
-end
-
-
 class CommentForm
   include React::Component
 
   def render
-    React.create_element('form') do
+    React.create_element('form', class_name: "commentForm") do
       [
         React.create_element('input', type: "text", placeholder: "Your name", ref: "author"),
         React.create_element('input', type: "text", placeholder: "Say something...", ref: "text"),
@@ -89,6 +56,49 @@ class CommentForm
   end
 end
 
+class CommentBox
+  include React::Component
+  after_mount :load_comments_from_server, :start_polling
+  define_state(:data) { [] }
+
+  def load_comments_from_server
+    HTTP.get(params[:url]) do |response|
+      if response.ok?
+        self.data = JSON.parse(response.body)
+      else
+        puts "failed with status #{response.status_code}"
+      end
+    end
+  end
+  
+  def start_polling
+    Window.set_interval(2000) { load_comments_from_server }
+  end
+
+  def handle_comment_submit(comment)
+    comments = self.data
+    comments.push(comment)
+    self.data = comments
+    
+    HTTP.post(params[:url], payload: comment) do |response|
+      if response.ok?
+        self.data = JSON.parse(response.body)
+      else
+        puts "failed with status #{response.status_code}"
+      end
+    end
+  end
+
+  def render
+    React.create_element('div', class_name: "commentBox") do
+      [React.create_element('h1') { "Comments" },
+       React.create_element(CommentList, data: self.data),
+       React.create_element(CommentForm).on(:comment_submit) {|c| handle_comment_submit(c) }]
+    end
+  end
+end
+
+
 Document.ready? do 
-  React.render React.create_element(CommentBox, url: "comments.json"), Element.find('#content').get(0)
+  React.render React.create_element(CommentBox, url: "comments.json", poll_interval: 2000), Element.find('#content').get(0)
 end
