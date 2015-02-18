b14abfe67caa8f7e5ef735661619d1c5929ad9c6
David Chang <zeta11235813@gmail.com>
2015-02-19 00:38:38 +0800

    Add isMounted() support

--- lib/react/component.rb
+++ lib/react/component.rb
@@ -1,11 +1,11 @@
 require "./ext/string"
 
 module React
-  module Component            
-    def self.included(base)  
+  module Component
+    def self.included(base)
       base.class_eval do
         def self.class_attribute(*attrs)
-          class << attrs 
+          class << attrs
             def extract_options!
               if last.is_a?(Hash) && last.extractable_options?
                 pop
@@ -62,35 +62,39 @@ module React
             attr_writer name if instance_writer
           end
         end
-      
+
         class_attribute :before_mount_callbacks, :after_mount_callbacks, :init_state, :cached_component_class
       end
       base.extend(ClassMethods)
     end
-    
+
     def initialize(native_element)
       @native = native_element
     end
-    
+
     def params
       Native(`#{@native}.props`)
     end
-    
+
     def refs
       Native(`#{@native}.refs`)
     end
-    
+
     def emit(event_name, *args)
       self.params["_on#{event_name.to_s.event_camelize}"].call(*args)
     end
-    
+
+    def mounted?
+      `#{@native}.isMounted()`
+    end
+
     def component_will_mount
       return unless self.class.before_mount_callbacks
       self.class.before_mount_callbacks.each do |callback|
         send(callback)
       end
     end
-    
+
     def component_did_mount
       return unless self.class.after_mount_callbacks
       self.class.after_mount_callbacks.each do |callback|
@@ -102,11 +106,11 @@ module React
       unless (React::HTML_TAGS.include?(name) || name == 'present')
         return super
       end
-      
+
       if name == "present"
         name = args.shift
-      end 
-      
+      end
+
       if block
         @buffer = [] unless @buffer
         current = @buffer
@@ -114,29 +118,29 @@ module React
         result = block.call
         element = React.create_element(name, *args) { @buffer.count == 0 ? result : @buffer }
         @buffer = current
-      else        
+      else
         element = React.create_element(name, *args)
       end
-      
+
       @buffer << element
       element
     end
-    
-    
+
+
     module ClassMethods
       def before_mount(*callback)
         self.before_mount_callbacks=  callback
       end
-      
+
       def after_mount(*callback)
         self.after_mount_callbacks = callback
       end
-      
+
       def define_state(*states)
-        raise "Block could be only given when define exactly one state" if block_given? && states.count > 1      
-        
+        raise "Block could be only given when define exactly one state" if block_given? && states.count > 1
+
         self.init_state = {} unless self.init_state
-        
+
         if block_given?
           self.init_state[states[0]] = yield
         end
@@ -144,7 +148,7 @@ module React
           # getter
           define_method("#{name}") do
             unless @native
-              self.class.init_state[name] 
+              self.class.init_state[name]
             else
               `#{@native}.state[#{name}]`
             end
@@ -160,7 +164,7 @@ module React
                 #{@native}.setState(state);
               }
             end
-            
+
             new_state
           end
           # setter with callback
@@ -176,10 +180,10 @@ module React
                 });
               }
             end
-            
+
             new_state
           end
-          
+
         end
       end
     end
--- spec/component_spec.rb
+++ spec/component_spec.rb
@@ -4,7 +4,7 @@ describe React::Component do
   after(:each) do
     React::API.clear_component_class_cache
   end
-  
+
   describe "Life Cycle" do
     before(:each) do
       stub_const 'Foo', Class.new
@@ -15,20 +15,20 @@ describe React::Component do
         end
       end
     end
-    
-    it "should invoke `before_mount` registered methods when `componentWillMount()`" do        
+
+    it "should invoke `before_mount` registered methods when `componentWillMount()`" do
       Foo.class_eval do
         before_mount :bar, :bar2
         def bar; end
         def bar2; end
       end
-      
+
       expect_any_instance_of(Foo).to receive(:bar)
       expect_any_instance_of(Foo).to receive(:bar2)
-      
+
       renderToDocument(Foo)
     end
-    
+
     it "should invoke `after_mount` registered methods when `componentDidMount()`" do
       Foo.class_eval do
         after_mount :bar3, :bar4
@@ -41,14 +41,14 @@ describe React::Component do
 
       renderToDocument(Foo)
     end
-    
+
     it "should allow multiple class declared life cycle hooker" do
       stub_const 'FooBar', Class.new
       Foo.class_eval do
         before_mount :bar
         def bar; end
       end
-      
+
       FooBar.class_eval do
         include React::Component
         after_mount :bar2
@@ -57,13 +57,13 @@ describe React::Component do
           React.create_element("div") { "lorem" }
         end
       end
-      
+
       expect_any_instance_of(Foo).to receive(:bar)
 
       renderToDocument(Foo)
     end
   end
-  
+
   describe "State setter & getter" do
     before(:each) do
       stub_const 'Foo', Class.new
@@ -74,7 +74,7 @@ describe React::Component do
         end
       end
     end
-    
+
     it "should define setter using `define_state`" do
       Foo.class_eval do
         define_state :foo
@@ -83,20 +83,20 @@ describe React::Component do
           self.foo = "bar"
         end
       end
-      
+
       element = renderToDocument(Foo)
       expect(element.state.foo).to be("bar")
     end
-    
+
     it "should define init state by passing a block to `define_state`" do
       Foo.class_eval do
         define_state(:foo) { 10 }
       end
-      
+
       element = renderToDocument(Foo)
       expect(element.state.foo).to be(10)
     end
-    
+
     it "should define getter using `define_state`" do
       Foo.class_eval do
         define_state(:foo) { 10 }
@@ -105,11 +105,11 @@ describe React::Component do
           self.foo = self.foo + 20
         end
       end
-      
+
       element = renderToDocument(Foo)
       expect(element.state.foo).to be(30)
     end
-    
+
     it "should define multiple state accessor by passing symols array to `define_state`" do
       Foo.class_eval do
         define_state :foo, :foo2
@@ -119,31 +119,31 @@ describe React::Component do
           self.foo2 = 20
         end
       end
-      
+
       element = renderToDocument(Foo)
       expect(element.state.foo).to be(10)
       expect(element.state.foo2).to be(20)
     end
-    
+
     it "should invoke `define_state` multiple times to define states" do
       Foo.class_eval do
         define_state(:foo) { 30 }
         define_state(:foo2) { 40 }
       end
-      
+
       element = renderToDocument(Foo)
       expect(element.state.foo).to be(30)
       expect(element.state.foo2).to be(40)
     end
-    
+
     it "should raise error if multiple states and block given at the same time" do
-      expect  { 
+      expect  {
         Foo.class_eval do
           define_state(:foo, :foo2) { 30 }
         end
       }.to raise_error
     end
-    
+
     it "should get state in render method" do
       Foo.class_eval do
         define_state(:foo) { 10 }
@@ -151,11 +151,11 @@ describe React::Component do
           React.create_element("div") { self.foo }
         end
       end
-      
+
       element = renderToDocument(Foo)
       expect(element.getDOMNode.textContent).to eq("10")
     end
-    
+
     pending "should set initial state in Class#initialize method" do
       Foo.class_eval do
         define_state :foo, :bar
@@ -164,12 +164,12 @@ describe React::Component do
           self.bar = 20
         end
       end
-      
+
       element = renderToDocument(Foo)
       expect(element.state.foo).to eq(10)
       expect(element.state.bar).to eq(20)
     end
-    
+
     pending "should allow getter for initial state in Class#initialize method" do
       Foo.class_eval do
         define_state :foo, :bar
@@ -178,13 +178,13 @@ describe React::Component do
           self.bar = self.foo + 20
         end
       end
-      
+
       element = renderToDocument(Foo)
       expect(element.state.foo).to eq(10)
       expect(element.state.bar).to eq(30)
     end
   end
-  
+
   describe "this.props could be accessed through `params` method" do
     before do
       stub_const 'Foo', Class.new
@@ -192,19 +192,19 @@ describe React::Component do
         include React::Component
       end
     end
-    
+
     it "should read from parent passed properties through `params`" do
       Foo.class_eval do
         def render
           React.create_element("div") { params[:prop] }
         end
       end
-      
+
       element = renderToDocument(Foo, prop: "foobar")
       expect(element.getDOMNode.textContent).to eq("foobar")
     end
   end
-  
+
   describe "Event handling" do
     before do
       stub_const 'Foo', Class.new
@@ -212,63 +212,63 @@ describe React::Component do
         include React::Component
       end
     end
-    
+
     it "should work in render method" do
       Foo.class_eval do
         define_state(:clicked) { false }
-        
+
         def render
           React.create_element("div").on(:click) do
             self.clicked = true
           end
         end
       end
-      
+
       element = React.create_element(Foo)
       instance = renderElementToDocument(element)
       simulateEvent(:click, instance)
       expect(instance.state.clicked).to eq(true)
     end
-    
+
     it "should invoke handler on `this.props` using emit" do
       Foo.class_eval do
         after_mount :setup
-        
+
         def setup
           self.emit(:foo_submit, "bar")
         end
-        
+
         def render
           React.create_element("div")
         end
       end
-      
+
       expect { |b|
         element = React.create_element(Foo).on(:foo_submit, &b)
         renderElementToDocument(element)
       }.to yield_with_args("bar")
     end
-    
+
     it "should invoke handler with multiple params using emit" do
       Foo.class_eval do
         after_mount :setup
-        
+
         def setup
           self.emit(:foo_invoked, [1,2,3], "bar")
         end
-        
+
         def render
           React.create_element("div")
         end
       end
-      
+
       expect { |b|
         element = React.create_element(Foo).on(:foo_invoked, &b)
         renderElementToDocument(element)
       }.to yield_with_args([1,2,3], "bar")
     end
   end
-  
+
   describe "Refs" do
     before do
       stub_const 'Foo', Class.new
@@ -276,47 +276,47 @@ describe React::Component do
         include React::Component
       end
     end
-    
+
     it "should correctly assign refs" do
       Foo.class_eval do
         def render
           React.create_element("input", type: :text, ref: :field)
         end
       end
-      
+
       element = renderToDocument(Foo)
       expect(element.refs.field).not_to be_nil
     end
-    
+
     it "should access refs through `refs` method" do
-      Foo.class_eval do    
+      Foo.class_eval do
         def render
           React.create_element("input", type: :text, ref: :field).on(:click) do
             refs[:field].value = "some_stuff"
           end
         end
       end
-      
+
       element = renderToDocument(Foo)
       simulateEvent(:click, element)
-      
+
       expect(element.refs.field.value).to eq("some_stuff")
     end
   end
-  
+
   describe "Render" do
     it "should support React::ElementBuilder helpers" do
       stub_const 'Foo', Class.new
       Foo.class_eval do
         include React::Component
-        
+
         def render
           div do
             span { params[:foo] }
           end
         end
       end
-      
+
       stub_const 'Bar', Class.new
       Bar.class_eval do
         include React::Component
@@ -326,8 +326,33 @@ describe React::Component do
           end
         end
       end
-      
+
       expect(React.render_to_static_markup(React.create_element(Bar))).to eq("<div><div><span>astring</span></div></div>")
+    end
+  end
+
+  describe "isMounted()" do
+    it "should return true if after mounted" do
+      stub_const 'Foo', Class.new
+      Foo.class_eval do
+        include React::Component
+
+        define_state(:mounted) { false }
+        after_mount :verify
+
+        def verify
+          if self.mounted?
+            self.mounted = true
+          end
+        end
+
+        def render
+          React.create_element("div")
+        end
+      end
+
+      element = renderToDocument(Foo)
+      expect(element.state.mounted).to eq(true)
     end
   end
 end
