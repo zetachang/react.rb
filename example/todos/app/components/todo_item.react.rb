32b03e0022e67a8544c4fdc36292b1e1ee3d67b8
David Chang <zeta11235813@gmail.com>
2015-02-18 23:46:03 +0800

    Update using classSet

--- example/todos/app/components/todo_item.react.rb
+++ example/todos/app/components/todo_item.react.rb
@@ -1,15 +1,15 @@
 class TodoItem
   include React::Component
-  
+
   define_state(:editing) { false }
   define_state(:edit_text)
-  
+
   before_mount :set_up
-  
+
   def set_up
     self.edit_text = params[:todo].title
   end
-  
+
   def finish_editing
     self.editing = false
     new_value = self.edit_text.strip
@@ -19,17 +19,17 @@ class TodoItem
       params[:todo].update(title: new_value)
     end
   end
-  
+
   def render
-    li(class_name: "#{self.editing ? 'editing' : ''}") do
+    li(class_name: {editing: self.editing}) do
       div(class_name: 'view') do
         input(class_name: "toggle", type: "checkbox", checked: params[:todo].completed).on(:click) do
           todo = params[:todo]
           todo.update(:completed => !todo.completed)
         end
-        label { self.edit_text }.on(:double_click) do 
+        label { self.edit_text }.on(:double_click) do
           # set on state will trigger re-render, so we manipulate the DOM after render done
-          self.set_editing(true) do 
+          self.set_editing(true) do
             self.refs.input.getDOMNode.focus
           end
           self.edit_text = params[:todo].title
