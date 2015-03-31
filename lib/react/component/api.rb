9cad22d72f21173c012a7066006636a25f94bb10
David Chang <zeta11235813@gmail.com>
2015-03-31 23:23:34 +0800

    Props is immutable now

--- lib/react/component/api.rb
+++ lib/react/component/api.rb
@@ -8,24 +8,6 @@ module React
       def force_update!
         `#{self}.forceUpdate()`
       end
-      
-      def set_props(prop, &block)
-        raise "No native ReactComponent associated" unless @native
-        %x{
-          #{self}.setProps(#{prop.shallow_to_n}, function(){
-            #{block.call if block}
-          });
-        }
-      end
-
-      def set_props!(prop, &block)
-        raise "No native ReactComponent associated" unless @native
-        %x{
-          #{self}.replaceProps(#{prop.shallow_to_n}, function(){
-            #{block.call if block}
-          });
-        }
-      end
 
       def set_state(state, &block)
         raise "No native ReactComponent associated" unless @native
