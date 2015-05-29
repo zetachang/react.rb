dea8688ab4f3794c53f448645f9ef87a8d3a4642
Mitch VanDuyn <mitch@catprint.com>
2015-05-29 14:16:03 -0400

    some missing parameter info in the react_component method

--- lib/rails-helpers/react_component.rb
+++ lib/rails-helpers/react_component.rb
@@ -8,7 +8,7 @@ begin
 
         alias_method :js_react_component, :react_component
 
-        def react_component(module_style_name, props, render_options={})
+        def react_component(module_style_name, props = {}, render_options={}, &block)
           js_name = module_style_name.gsub("::", ".") 
           js_react_component(js_name, props, render_options)
         end
