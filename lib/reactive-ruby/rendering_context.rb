3fd7e8f61cb50e8fe6f6899e5988abe0d4a1944d
Mitch VanDuyn <mitch@catprint.com>
2015-09-04 12:43:46 -0400

    last change was not working with dangerously_set_inner_html

--- lib/reactive-ruby/rendering_context.rb
+++ lib/reactive-ruby/rendering_context.rb
@@ -73,7 +73,7 @@ module React
     
     def self.remove_nodes_from_args(args)
       args[0].each do |key, value|
-        value.as_node if value.is_a? Element
+        value.as_node if value.is_a? Element rescue nil
       end if args[0] and args[0].is_a? Hash
     end
     
--- lib/reactive-ruby/version.rb
+++ lib/reactive-ruby/version.rb
@@ -1,3 +1,3 @@
 module React
-  VERSION = "0.7.14"
+  VERSION = "0.7.15"
 end
