d972281a3a865e35045d5705996303886c4574f6
Adam Jahn <ajjahn@gmail.com>
2015-10-14 21:19:35 -0400

    cleanup and specs for contextual renderer

--- lib/reactive-ruby/server_rendering/contextual_renderer.rb
+++ lib/reactive-ruby/server_rendering/contextual_renderer.rb
@@ -1,77 +1,41 @@
 module ReactiveRuby
   module ServerRendering
-    class ContextualRenderer < React::ServerRendering::ExecJSRenderer
+    class ContextualRenderer < React::ServerRendering::SprocketsRenderer
       def initialize(options = {})
-        @replay_console = options.fetch(:replay_console, true)
-        filenames = options.fetch(:files, ["react.js", "components.js"])
-        js_code = CONSOLE_POLYFILL.dup
-
-        filenames.each do |filename|
-          js_code << ::Rails.application.assets[filename].to_s
-        end
-
-        super(options.merge(code: js_code))
+        super(options)
+        load_components
       end
 
       def render(component_name, props, prerender_options)
         if prerender_options.is_a? Hash
-          if ExecJS.runtime.name == "(V8)" and prerender_options[:context_initializer]
-            raise React::ServerRendering::PrerenderError.new(component_name, props, "you must use 'therubyracer' with the prerender[:context] option") unless ExecJS.runtime.name == "(V8)"
+          if v8_runtime? and prerender_options[:context_initializer]
+            raise React::ServerRendering::PrerenderError.new(component_name, props, "you must use 'therubyracer' with the prerender[:context] option") unless v8_runtime?
           else
-            prerender_options[:context_initializer].call @context.instance_variable_get("@v8_context")
+            prerender_options[:context_initializer].call v8_context
             prerender_options = prerender_options[:static] ? :static : true
           end
         end
-        #pass prerender: :static to use renderToStaticMarkup
-        react_render_method = if prerender_options == :static
-                                "renderToStaticMarkup"
-                              else
-                                "renderToString"
-                              end
 
-        if !props.is_a?(String)
-          props = props.to_json
-        end
-
-        js_code = <<-JS
-          (function () {
-            var result = React.#{react_render_method}(React.createElement(#{component_name}, #{props}));
-        #{@replay_console ? CONSOLE_REPLAY : ""}
-            return result;
-          })()
-        JS
-
-        @context.eval(js_code).html_safe
-      rescue ExecJS::ProgramError => err
-        raise React::ServerRendering::PrerenderError.new(component_name, props, err)
+        super(component_name, props, prerender_options)
       end
 
-      def after_render(component_name, props, prerender_options)
-        @replay_console ? CONSOLE_REPLAY : ""
+      private
+
+      def v8_runtime?
+        ExecJS.runtime.name == "(V8)"
       end
 
-      # Reimplement console methods for replaying on the client
-      CONSOLE_POLYFILL = <<-JS
-        var console = { history: [] };
-        ['error', 'log', 'info', 'warn'].forEach(function (fn) {
-          console[fn] = function () {
-            console.history.push({level: fn, arguments: Array.prototype.slice.call(arguments)});
-          };
-        });
-      JS
+      def v8_context
+        @v8_context ||= @context.instance_variable_get("@v8_context")
+      end
 
-      # Replay message from console history
-      CONSOLE_REPLAY = <<-JS
-        (function (history) {
-          if (history && history.length > 0) {
-            result += '\\n<scr'+'ipt>';
-            history.forEach(function (msg) {
-              result += '\\nconsole.' + msg.level + '.apply(console, ' + JSON.stringify(msg.arguments) + ');';
-            });
-            result += '\\n</scr'+'ipt>';
-          }
-        })(console.history);
-      JS
+      def load_components
+        v8_context.eval(processed_opal_asset)
+      end
+
+      def processed_opal_asset
+        Opal::Processor.load_asset_code(::Rails.application.assets,'components')
+      end
     end
   end
 end
