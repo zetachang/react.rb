388615594e04853aeda45e3d6ed851d4c8d5a362
Adam Jahn <ajjahn@gmail.com>
2015-12-16 00:04:57 -0500

    remove collect_other_params_as method define

--- lib/react/component/class_methods.rb
+++ lib/react/component/class_methods.rb
@@ -79,10 +79,6 @@ module React
 
       def collect_other_params_as(name)
         validator.allow_undefined_props = true
-        define_method(name) do
-          @_all_others ||= self.class.validator.undefined_props(props)
-        end
-
         validator_in_lexial_scope = validator
         props_wrapper.define_method(name) do
           @_all_others ||= validator_in_lexial_scope.undefined_props(props)
